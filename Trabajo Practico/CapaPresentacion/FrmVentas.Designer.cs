b2c6a4a72625a75bdc05de526133a7f4ef12a8b6
Guillermo De Dios <guille_dedios03@hotmail.com>
2022-11-15 15:04:30 -0300

    cambios

--- Trabajo Practico/CapaPresentacion/FrmVentas.Designer.cs
+++ Trabajo Practico/CapaPresentacion/FrmVentas.Designer.cs
@@ -185,6 +185,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(676, 236);
             this.dataGridView1.TabIndex = 36;
             // 
